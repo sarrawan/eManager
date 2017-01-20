f0b15d83a4b18a893c7596ce93cf452a701eb11f
RawanS <rawansar96@gmail.com>
2017-01-19 16:56:56 -0800

    Add project files.

---
+++ eManager.Domain/IDepartmentDataSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eManager.Domain
+{
+    public interface IDepartmentDataSource
+    {
+        IQueryable<Employee> Employees { get;}
+        IQueryable<Department> Departments { get; }
+    } 
+}
---
+++ eManager.Web/Infrastructure/DepartmentDb.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using eManager.Domain;
+
+namespace eManager.Web.Infrastructure
+{
+    public class DepartmentDb : DbContext, IDepartmentDataSource
+    {
+        public DepartmentDb() : base("DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        IQueryable<Employee> IDepartmentDataSource.Employees
+        {
+            get
+            {
+                return Employees;
+            }
+        }
+
+        IQueryable<Department> IDepartmentDataSource.Departments
+        {
+            get
+            {
+                return Departments;
+            }
+        }
+    }
+}
